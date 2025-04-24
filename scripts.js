a1ff502a464ade2ff53e2c8b3143964cac01d4ec
[Hugofr] <[hferrod@aulaestudio.es]>
2025-04-24 19:20:21 +0200

    commit

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('#toggleMode').click(function () {
-      $('body').toggleClass('dark-mode');
+      $('body').toggleClass('modooscuro');
   
       const isDark = $('body').hasClass('dark-mode');
       $(this).text(isDark ? '☀️' : '🌙');

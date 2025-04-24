20682161f7fb31f71cfd436190999dcae3839cd8
[Hugofr] <[hferrod@aulaestudio.es]>
2025-04-24 19:36:45 +0200

    commit

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('#toggleMode').click(function () {
       $('body').toggleClass('modooscuro');
   
-      const isDark = $('body').hasClass('dark-mode');
+      const isDark = $('body').hasClass('modooscuro');
       $(this).text(isDark ? '☀️' : '🌙');
     });
   });
